4bbbd05248b3eae9ba33cb90f886ecf3ff575d70
Jesse MacFadyen <purplecabbage@gmail.com>
2016-12-31 23:44:02 -0800

    handle apk files too

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function outputResults(res) {
     resultsWindow.on('closed', function () {
         resultsWindow = null;
     });
-    dropZone.innerText = "Drop an ipa file here to inspect it.";
+    dropZone.innerText = "Drop some .ipa or .apk files here to inspectigate.";
 
 }
 
@@ -47,27 +47,42 @@ function processFiles(files) {
     var isIPA = function(file) {
         return path.extname(file.path ? file.path : file) === ".ipa";
     };
-    var fileQueue;
-    if(files.filter) {
-        fileQueue = files.filter(isIPA);
+    var isAPK = function(file) {
+        return path.extname(file.path ? file.path : file) === ".apk";
     }
-    else {
-       fileQueue = Array.prototype.filter.call(files,isIPA);
-    }
+    var ipaQueue;
 
+    var filterFunk = files.filter || function (funk) {
+        return Array.prototype.filter.call(files,funk);
+    };
 
-    if(fileQueue.length > 0) {
-        dropZone.innerText = "Processing " + fileQueue.length + " files ...";
+    ipaQueue = filterFunk(isIPA);
+    apkQueue = filterFunk(isAPK);
+
+    if(ipaQueue.length > 0) {
+        dropZone.innerText = "Processing " + ipaQueue.length + " ipa files ...";
         let workerPath = 'file://' + path.join(__dirname, '/worker.html');
         workerWindow = new BrowserWindow({ width: 400, height: 400, show: false });
         workerWindow.loadURL(workerPath);
 
         workerWindow.webContents.once('did-finish-load', function () {
-            workerWindow.webContents.send('process-files', fileQueue, myWindowId);
+            workerWindow.webContents.send('process-files', ipaQueue, myWindowId);
         });
     }
-    else {
-        window.alert("oops, none of the files were .ipa files.")
+
+    if(apkQueue.length > 0) {
+        dropZone.innerText = "Processing " + apkQueue.length + " apk files ...";
+        let workerPath = 'file://' + path.join(__dirname, '/worker.html');
+        workerWindow = new BrowserWindow({ width: 400, height: 400, show: false });
+        workerWindow.loadURL(workerPath);
+
+        workerWindow.webContents.once('did-finish-load', function () {
+            workerWindow.webContents.send('process-files', apkQueue, myWindowId);
+        });
+    }
+
+    if(ipaQueue.length.length < 1 && apkQueue.length < 1) {
+        window.alert("oops, none of the files were .ipa or .apk files.")
     }
 }
 
